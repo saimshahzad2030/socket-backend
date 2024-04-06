3f1da48c0ab60ead7c450d0ce35a3915cd76117b
Mirza Saim Shahzad <51031970+saimshahzad2030@users.noreply.github.com>
2024-04-06 20:25:44 +0500

    final

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http'); // Import the HTTP module
-const WebSocket = require('ws'); // Import the WebSocket module
-
-const app = express();
-const PORT = 5000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", async (req, res) => {
+const express = require('express')
+const cors = require('cors')
+const {Server, Socket} = require('socket.io')
+const app = express()
+const PORT = 5000
+app.use(cors())
+app.use(express.json())
+app.get("/",async (req, res) => {
   res.json('Hello World');
 });
-
-const server = http.createServer(app); // Create an HTTP server
-const wss = new WebSocket.Server({ server }); // Create a WebSocket server
-
-let likes = 0;
-
-wss.on('connection', (ws) => {
-  ws.send(JSON.stringify({ event: 'likeUpdate', data: likes })); // Send initial like count when connected
-
-  ws.on('message', (message) => {
-    if (message === 'liked') {
-      likes++;
-      // Broadcast the updated like count to all connected clients
-      wss.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ event: 'likeUpdate', data: likes }));
-        }
-      });
-    }
-  });
-});
-
-server.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running at PORT ${PORT}`);
 });
+
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+let likes = 0;
+io.on('connect',(socket)=>{
+  socket.emit('likeUpdate',likes)
+  socket.on('liked',()=>{
+    likes++;
+    socket.emit('likeUpdate',likes);
+    socket.broadcast.emit('likeUpdate',likes)
+  })
+})
+
+
+// io.listen(PORT, () => console.log(`Server runing at PORT ${PORT}`));
+// module.exports = {io}
