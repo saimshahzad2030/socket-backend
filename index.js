31798f42462c8914f983caae2844aecc2bb5bd52
Mirza Saim Shahzad <51031970+saimshahzad2030@users.noreply.github.com>
2024-04-06 09:45:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const {Server, Socket} = require('socket.io')
+const app = express()
+const PORT = 5000
+app.use(cors())
+app.use(express.json())
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running at PORT ${PORT}`);
+});
+
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+let likes = 0;
+io.on('connect',(socket)=>{
+  socket.emit('likeUpdate',likes)
+  socket.on('liked',()=>{
+    likes++;
+    socket.emit('likeUpdate',likes);
+    socket.broadcast.emit('likeUpdate',likes)
+  })
+})
+app.get("/",async (req, res) => {
+  res.json('Hello World');
+});
+
+// io.listen(PORT, () => console.log(`Server runing at PORT ${PORT}`));
